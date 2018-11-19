3d8414ab666187e98ff34b0890cb1bba668e0a05
[jackshen] <[736201041@qq.com]]>
2018-11-20 00:00:21 +0800

    修改

--- src/main/java/com/winterchen/test/Test.java
+++ src/main/java/com/winterchen/test/Test.java
@@ -3,6 +3,7 @@ package com.winterchen.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Test");
+        System.out.println("真的很好");
         System.out.println("你好啊");
     }
 }
